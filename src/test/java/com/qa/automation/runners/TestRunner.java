150a5bc495912ab0acf202f4c6e6341d37a84565
sarangholey <sarangholey@gmail.com>
2023-09-23 09:24:59 +0530

    For PR No 2 Changes made

--- src/test/java/com/qa/automation/runners/TestRunner.java
+++ src/test/java/com/qa/automation/runners/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
         features="classpath:features",//to tell cucumber where is ur feature file
         glue="com.qa.automation.stepdefs", // to tell cucumber where is ur step def code
-        tags="", // to tell which tagged feature file to execute
+        tags="", // to tell which tagged feature file to execute 
         plugin = {"pretty", // to generate reports
             "html:target/html/web_ui_automation_report.html",
             "json:target/json/json_report.json",
